b8e06bbe7022a3c551b6edd38fe08fd0b59ee056
Felipe A. V. de Bragança Alves <favbalves@gmail.com>
2025-03-19 14:06:57 -0300

    Reduce code repetition.

--- ext/GeometryBasicsExt.jl
+++ ext/GeometryBasicsExt.jl
@@ -11,42 +11,46 @@ else
     const PolType = Polygon{2, Float32}
 end
 
-function create_cell_polygons_periodic(vert_pos, cell_pos, verticesOnCell, x_period, y_period)
+function create_polygons_periodic(vert_pos, polygon_pos, verticesOnPolygon, x_period, y_period)
 
-    cell_polygons = Vector{PolType}(undef, length(cell_pos))
-    NE = max_length(eltype(verticesOnCell))
-    @parallel for i in eachindex(cell_pos)
+    polygons = Vector{PolType}(undef, length(polygon_pos))
+    NE = max_length(eltype(verticesOnPolygon))
+    @parallel for i in eachindex(polygon_pos)
         @inbounds begin
-            cpos = cell_pos[i]
+            ppos = polygon_pos[i]
             local_vertices = ImmutableVector{NE, Point2f}()
-            for i_v in verticesOnCell[i]
-                vpos = closest(cpos, vert_pos[i_v], x_period, y_period)
+            for i_v in verticesOnPolygon[i]
+                vpos = closest(ppos, vert_pos[i_v], x_period, y_period)
                 local_vertices = @inbounds push(local_vertices, Point2f(vpos.x, vpos.y))
             end
-            cell_polygons[i] = Polygon(Array(local_vertices))
+            polygons[i] = Polygon(Array(local_vertices))
         end
     end
 
-    return cell_polygons
+    return polygons
 end
 
 function VoronoiMeshes.create_cell_polygons(mesh::AbstractVoronoiMesh{false})
-    return create_cell_polygons_periodic(mesh.vertices.position, mesh.cells.position, mesh.cells.vertices, mesh.x_period, mesh.y_period)
+    return create_polygons_periodic(mesh.vertices.position, mesh.cells.position, mesh.cells.vertices, mesh.x_period, mesh.y_period)
 end
 
-function create_cell_polygons_sphere(vert_lon::Vector{T}, vert_lat, cell_lon, verticesOnCell, clip::Bool = false) where {T<:Number}
+function VoronoiMeshes.create_dual_triangles(mesh::AbstractVoronoiMesh{false})
+    return create_polygons_periodic(mesh.cells.position, mesh.vertices.position, mesh.vertices.cells, mesh.x_period, mesh.y_period)
+end
+
+function create_polygons_sphere(vert_lon::Vector{T}, vert_lat, base_lon, verticesOnElement, clip::Bool = false) where {T<:Number}
     x_period = T(360)
 
-    cell_polygons = Vector{PolType}(undef, length(cell_lon))
-    NE = max_length(eltype(verticesOnCell))
+    polygons = Vector{PolType}(undef, length(base_lon))
+    NE = max_length(eltype(verticesOnElement))
 
-    @parallel for i in eachindex(cell_lon)
+    @parallel for i in eachindex(base_lon)
         @inbounds begin
-            c_lon_aux = rad2deg(cell_lon[i])
+            c_lon_aux = rad2deg(base_lon[i])
             c_lon = c_lon_aux > 180 ? c_lon_aux - x_period : c_lon_aux
             local_vertices = ImmutableVector{NE, Point2f}()
 
-            for i_v in verticesOnCell[i]
+            for i_v in verticesOnElement[i]
                 vlon_aux_1 = rad2deg(vert_lon[i_v])
                 vlon_aux = vlon_aux_1 > 180 ? vlon_aux_1 - x_period : vlon_aux_1
                 vlat = rad2deg(vert_lat[i_v])
@@ -74,41 +78,19 @@ function create_cell_polygons_sphere(vert_lon::Vector{T}, vert_lat, cell_lon, ve
 
                 local_vertices = @inbounds push(local_vertices, Point2f(vlon, vlat))
             end
-            cell_polygons[i] = Polygon(Array(local_vertices))
+            polygons[i] = Polygon(Array(local_vertices))
         end
     end
 
-    return cell_polygons
+    return polygons
 end
 
 function VoronoiMeshes.create_cell_polygons(mesh::AbstractVoronoiMesh{true}, clip::Bool = false)
-    return create_cell_polygons_sphere(mesh.vertices.longitude, mesh.vertices.latitude, mesh.cells.longitude, mesh.cells.vertices, clip)
+    return create_polygons_sphere(mesh.vertices.longitude, mesh.vertices.latitude, mesh.cells.longitude, mesh.cells.vertices, clip)
 end
 
-function create_dual_triangles_periodic(vert_pos, cell_pos, cellsOnVertex, x_period, y_period)
-
-    vert_triangles = Vector{PolType}(undef, length(vert_pos))
-
-    @parallel for i in eachindex(vert_pos)
-        @inbounds begin
-            vpos = vert_pos[i]
-            ic1, ic2, ic3 = cellsOnVertex[i]
-
-            cpos1 = closest(vpos, cell_pos[ic1], x_period, y_period)
-
-            cpos2 = closest(vpos, cell_pos[ic2], x_period, y_period)
-
-            cpos3 = closest(vpos, cell_pos[ic3], x_period, y_period)
-
-            vert_triangles[i] = Polygon([Point2f(cpos1.x, cpos1.y), Point2f(cpos2.x, cpos2.y), Point2f(cpos3.x, cpos3.y)])
-        end
-    end
-
-    return vert_triangles
-end
-
-function VoronoiMeshes.create_dual_triangles(mesh::AbstractVoronoiMesh{false})
-    return create_dual_triangles_periodic(mesh.vertices.position, mesh.cells.position, mesh.vertices.cells, mesh.x_period, mesh.y_period)
+function VoronoiMeshes.create_dual_triangles(mesh::AbstractVoronoiMesh{true}, clip::Bool = false)
+    return create_polygons_sphere(mesh.cells.longitude, mesh.cells.latitude, mesh.vertices.longitude, mesh.vertices.cells, clip)
 end
 
 function create_edge_quadrilaterals_periodic(edge_pos, vert_pos, cell_pos, verticesOnEdge, cellsOnEdge, x_period, y_period)
@@ -140,7 +122,7 @@ function VoronoiMeshes.create_edge_quadrilaterals(mesh::AbstractVoronoiMesh{fals
     return create_edge_quadrilaterals_periodic(mesh.edges.position, mesh.vertices.position, mesh.cells.position, mesh.edges.vertices, mesh.edges.cells, mesh.x_period, mesh.y_period)
 end
 
-function create_cell_linesegments_periodic(vert_pos, edge_pos, cell_pos, edgesOnCell, verticesOnEdge, x_period, y_period)
+function create_polygon_linesegments_periodic(vert_pos, edge_pos, polygon_pos, edgesOnPolygon, verticesOnEdge, x_period, y_period)
     x = eltype(edge_pos.x)[]
     y = eltype(edge_pos.y)[]
     nEdges = length(verticesOnEdge)
@@ -153,9 +135,9 @@ function create_cell_linesegments_periodic(vert_pos, edge_pos, cell_pos, edgesOn
 
     touched_edges_pos = Set{eltype(edge_pos)}()
 
-    @inbounds for i in eachindex(edgesOnCell)
-        c_pos = cell_pos[i]
-        edges_ind = edgesOnCell[i]
+    @inbounds for i in eachindex(edgesOnPolygon)
+        c_pos = polygon_pos[i]
+        edges_ind = edgesOnPolygon[i]
 
         for j in edges_ind
             e_pos = edge_pos[j]
@@ -187,7 +169,11 @@ function create_cell_linesegments_periodic(vert_pos, edge_pos, cell_pos, edgesOn
 end
 
 function VoronoiMeshes.create_cell_linesegments(mesh::AbstractVoronoiMesh{false})
-    return create_cell_linesegments_periodic(mesh.vertices.position, mesh.edges.position, mesh.cells.position, mesh.cells.edges, mesh.edges.vertices, mesh.x_period, mesh.y_period)
+    return create_polygon_linesegments_periodic(mesh.vertices.position, mesh.edges.position, mesh.cells.position, mesh.cells.edges, mesh.edges.vertices, mesh.x_period, mesh.y_period)
+end
+
+function VoronoiMeshes.create_dual_triangles_linesegments(mesh::AbstractVoronoiMesh{false})
+    return create_polygon_linesegments_periodic(mesh.cells.position, mesh.edges.position, mesh.vertices.position, mesh.vertices.edges, mesh.edges.cells, mesh.x_period, mesh.y_period)
 end
 
 function create_diagram_linesegments_periodic(vert_pos, cell_pos, verticesOnCell, cellsOnVertex, x_period, y_period)
@@ -227,56 +213,6 @@ end
 
 function VoronoiMeshes.create_diagram_linesegments(diagram::VoronoiDiagram{false})
     return create_diagram_linesegments_periodic(diagram.vertices, diagram.generators, diagram.verticesOnCell, diagram.cellsOnVertex, diagram.x_period, diagram.y_period)
-end
-
-function create_dual_triangles_linesegments_periodic(vert_pos, edge_pos, cell_pos, edgesOnVertex, cellsOnEdge, x_period, y_period)
-    x = eltype(edge_pos.x)[]
-    y = eltype(edge_pos.y)[]
-    nEdges = length(cellsOnEdge)
-    sizehint!(x, 2 * nEdges)
-    sizehint!(y, 2 * nEdges)
-    x_periodic = eltype(edge_pos.x)[]
-    y_periodic = eltype(edge_pos.y)[]
-    sizehint!(x_periodic, nEdges ÷ 2)
-    sizehint!(y_periodic, nEdges ÷ 2)
-
-    touched_edges_pos = Set{eltype(edge_pos)}()
-
-    @inbounds for i in eachindex(edgesOnVertex)
-        v_pos = vert_pos[i]
-        edges_ind = edgesOnVertex[i]
-
-        for j in edges_ind
-            e_pos = edge_pos[j]
-            closest_e_pos = closest(v_pos, e_pos, x_period, y_period)
-            if !(closest_e_pos in touched_edges_pos)
-                p = cellsOnEdge[j]
-
-                is_interior = closest_e_pos == e_pos
-                for k in p
-                    c_pos = cell_pos[k]
-                    closest_c_pos = closest(closest_e_pos, c_pos, x_period, y_period)
-                    if (is_interior)
-                        push!(x, closest_c_pos.x)
-                        push!(y, closest_c_pos.y)
-                    else
-                        push!(x_periodic, closest_c_pos.x)
-                        push!(y_periodic, closest_c_pos.y)
-                    end
-                end
-
-                push!(touched_edges_pos, closest_e_pos)
-            end
-
-        end
-
-    end
-
-    return ((x, y), (x_periodic, y_periodic))
-end
-
-function VoronoiMeshes.create_dual_triangles_linesegments(mesh::AbstractVoronoiMesh{false})
-    return create_dual_triangles_linesegments_periodic(mesh.vertices.position, mesh.edges.position, mesh.cells.position, mesh.vertices.edges, mesh.edges.cells, mesh.x_period, mesh.y_period)
 end
 
 end

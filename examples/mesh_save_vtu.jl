d66c4636ff8c519a0fbc8471074b9048dbb88171
Pedro Peixoto <ppeixoto@usp.br>
2025-10-13 13:48:37 -0300

    added example to export vtu with triangles

--- examples/mesh_save_vtu.jl
+++ examples/mesh_save_vtu.jl
@@ -15,6 +15,17 @@ using WriteVTK # For saving meshes in VTU format
 # Create a centroidal Voronoi mesh with 20 cells on a 1×1 periodic domain
 mesh = VoronoiMesh(20, 1.0, 1.0)
 
+points = [mesh.cells.position.x'; mesh.cells.position.y'; mesh.cells.position.z']
+n = length(mesh.vertices.cells)
+triangles = Vector{MeshCell}(undef, n)
+Threads.@threads for i in 1:n
+    # allocate per-iteration data to avoid races
+    idx = collect(mesh.vertices.cells[i])
+    triangles[i] = MeshCell(VTKCellTypes.VTK_TRIANGLE, idx)
+end
+vtk = vtk_grid("test.vtu", points, triangles)
+saved_files = close(vtk)
+
 # Print a summary of the mesh
 println(mesh)
 

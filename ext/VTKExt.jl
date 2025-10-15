2fa8eb5b078d3cf5614ce7471b077ef31a8da64d
Pedro Peixoto <ppeixoto@usp.br>
2025-10-15 18:09:25 -0300

    [fix] added metadata to vtu file and ghost cell index tags

--- ext/VTKExt.jl
+++ ext/VTKExt.jl
@@ -54,22 +54,22 @@ function create_ghost_periodic_points(vert_pos, polygon_pos, verticesOnPolygon, 
     vertices_with_ghosts = copy(vert_pos)
     ivertices_with_ghosts = length(vert_pos) + 1
     verticesOnPolygon_with_ghosts = [Vector(v) for v in verticesOnPolygon]
-    d = Dict{Int, Vector{Int}}() # Save ghost index for each original vertex
+    ghost_dict = Dict{Int, Vector{Int}}() # Save ghost index for each original vertex
     for i in eachindex(polygon_pos)
         ppos = polygon_pos[i]
         for j in eachindex(verticesOnPolygon[i])
             original_index = verticesOnPolygon[i][j]
             vpos = closest(ppos, vert_pos[original_index], x_period, y_period)
             if norm(vpos-vert_pos[original_index]) > 0.4*min(x_period, y_period)
-                if !haskey(d, original_index) #first time we see this ghost
-                    d[original_index] = [ivertices_with_ghosts]
+                if !haskey(ghost_dict, original_index) #first time we see this ghost
+                    ghost_dict[original_index] = [ivertices_with_ghosts]
                     verticesOnPolygon_with_ghosts[i][j] = ivertices_with_ghosts
                     push!(vertices_with_ghosts, vpos)
                     ivertices_with_ghosts += 1
                     #println(i, " ", j, " Ghost found:", vpos, " for original vertex ", original_index, " at ", vert_pos[original_index], " Dict:", d[original_index])
                 else #in this case a key already exists, check if this vertex is close or not to existing ghosts
                     ghost_exists = false
-                    for k in d[original_index]
+                    for k in ghost_dict[original_index]
                         if norm(vpos-vertices_with_ghosts[k]) < 1e-8*min(x_period, y_period)
                             # use the existing ghost vertex
                             verticesOnPolygon_with_ghosts[i][j] = k
@@ -80,7 +80,7 @@ function create_ghost_periodic_points(vert_pos, polygon_pos, verticesOnPolygon, 
                     #New ghost vertex needed
                     if !ghost_exists
                         # add new ghost vertex
-                        push!(d[original_index], ivertices_with_ghosts)
+                        push!(ghost_dict[original_index], ivertices_with_ghosts)
                         verticesOnPolygon_with_ghosts[i][j] = ivertices_with_ghosts
                         push!(vertices_with_ghosts, vpos)
                         ivertices_with_ghosts += 1
@@ -91,8 +91,8 @@ function create_ghost_periodic_points(vert_pos, polygon_pos, verticesOnPolygon, 
         end
     end
     n_ghosts = length(vertices_with_ghosts) - length(vert_pos)
-    println("Added $n_ghosts ghost vertices to VTU to handle periodicity in mesh")
-    return vertices_with_ghosts, verticesOnPolygon_with_ghosts, n_ghosts
+    println("Added $n_ghosts ghost vertices to VTU to handle periodicity in mesh with ", length(vert_pos), " original vertices.")
+    return vertices_with_ghosts, verticesOnPolygon_with_ghosts, n_ghosts, ghost_dict
 end
 
 
@@ -103,17 +103,42 @@ end
 Export the Voronoi mesh to a VTU file, handling periodic ghost vertices.
 """
 function save_voronoi_to_vtu(file_name::String, mesh::AbstractVoronoiMesh{false})
-    vertices_with_ghosts, verticesOnPolygon_with_ghosts, _ =
+    # Here the vertices are Voronoi cell vertices and the polygons are the Voronoi cells
+    vertices_with_ghosts, verticesOnPolygon_with_ghosts, n_ghosts, ghost_dict =
         create_ghost_periodic_voronoi_vertices(mesh)
-    n_polys = mesh.cells.n
+    n_polys = mesh.cells.n #number of Voronoi cells
+    n_vertices = mesh.vertices.n #number of Voronoi cell vertices (matches number of triangles)
     points_mat = hcat((v[1:2] for v in vertices_with_ghosts)...)        
     cells = Vector{MeshCell}(undef, n_polys)
     for i in 1:n_polys
         links = collect(verticesOnPolygon_with_ghosts[i])
         cells[i] = MeshCell(VTKCellTypes.VTK_POLYGON, links)
     end
-    vtk = vtk_grid(file_name, points_mat, cells)
-    saved_file = close(vtk)
+        # Save ghost periodicity information
+
+    # Vertices from 1:n_polys will save their index 1:n_polys
+    # Ghost vertices will save the index of the original vertex they correspond to, negative
+    n_vertices_with_ghosts = length(vertices_with_ghosts)
+    ghost_idx = collect(1:n_vertices_with_ghosts)
+    for i in 1:n_vertices
+        if haskey(ghost_dict, i)
+            for j in ghost_dict[i]
+                ghost_idx[j] = -i
+            end
+        end
+    end
+    
+    # add mesh metadata
+    saved_file = vtk_grid(file_name, points_mat, cells) do vtk 
+        vtk["Index", VTKPointData()] = ghost_idx
+        vtk["NumCells"] = mesh.cells.n
+        vtk["NumVertices"] = mesh.vertices.n
+        vtk["NumEdges"] = mesh.edges.n
+        vtk["XPeriod"] = mesh.x_period
+        vtk["YPeriod"] = mesh.y_period
+        vtk["NumPeriodicGhosts"] = n_ghosts
+    end
+    
     return saved_file
 end
 
@@ -124,18 +149,46 @@ end
 Export the Delaunay triangulation (dual mesh) to a VTU file, handling periodic ghost vertices.
 """
 function save_triangulation_to_vtu(file_name::String, mesh::AbstractVoronoiMesh{false})
-    vertices_with_ghosts, verticesOnPolygon_with_ghosts, _ =
+    # Here the vertices are cell centers and the polygons are the triangles
+    vertices_with_ghosts, verticesOnPolygon_with_ghosts, n_ghosts, ghost_dict =
         create_ghost_periodic_triangulation_vertices(mesh)
-    n_polys = mesh.vertices.n
+
+    n_polys = mesh.vertices.n #number of mesh triangles
+    n_vertices = mesh.cells.n #number of triangle vertices
     points_mat = hcat((v[1:2] for v in vertices_with_ghosts)...)        
     cells = Vector{MeshCell}(undef, n_polys)
     for i in 1:n_polys
         links = collect(verticesOnPolygon_with_ghosts[i])
         cells[i] = MeshCell(VTKCellTypes.VTK_TRIANGLE, links)
     end
-    vtk = vtk_grid(file_name, points_mat, cells)
-    saved_file = close(vtk)
+
+    # Save ghost periodicity information
+
+    # Vertices from 1:n_polys will save their index 1:n_polys
+    # Ghost vertices will save the index of the original vertex they correspond to, negative
+    n_vertices_with_ghosts = length(vertices_with_ghosts)
+    ghost_idx = collect(1:n_vertices_with_ghosts)
+    for i in 1:n_vertices
+        if haskey(ghost_dict, i)
+            for j in ghost_dict[i]
+                ghost_idx[j] = -i
+            end
+        end
+    end
+    
+    # add mesh metadata
+    saved_file = vtk_grid(file_name, points_mat, cells) do vtk 
+        vtk["Index", VTKPointData()] = ghost_idx
+        vtk["NumCells"] = mesh.cells.n
+        vtk["NumVertices"] = mesh.vertices.n
+        vtk["NumEdges"] = mesh.edges.n
+        vtk["XPeriod"] = mesh.x_period
+        vtk["YPeriod"] = mesh.y_period
+        vtk["NumPeriodicGhosts"] = n_ghosts
+    end
+    
     return saved_file
 end
+
 
 end # module VTKExt
